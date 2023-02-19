53f2453f98db0a872bf7ad6e1bdaeae6a06d6656
Sunghee Park <sungheepark@Sunghees-MacBook-Pro.local>
2023-02-19 13:42:01 -0800

    updated

--- sales_analysis.R
+++ sales_analysis.R
@@ -6,28 +6,29 @@ library(ggplot2)
 library(plotly)
 
 
-# 2017 sales data
+# sales data in 2017
 sales_data_2017 <- sales_data_2017_2018 %>% 
   filter(year == 2017)
-
 # View(sales_data_2017)
 
-# 2018 sales data
+# sales data in 2018
 sales_data_2018 <- sales_data_2017_2018 %>% 
   filter(year == 2018)
 
 # View(sales_data_2018)
 
-# items data
+# items data (information distinct to each item)
 items_data <- sales_data_2017_2018 %>% 
   group_by(item_name) %>% 
   select(item_code, item_name, main_category, sub_category, unit_buying_price,
          unit_selling_price, unit_price_margin) %>% 
   distinct()
 
+# View(items_data)
+
+
 
 # summary for both 2017 & 2018
-
 # sales_count
 sales_count_2017_2018 <- sales_data_2017_2018 %>% 
   group_by(item_name) %>% 
@@ -47,8 +48,7 @@ sum_profit <- sales_data_2017_2018 %>%
 summary_2017_2018 <- sum_profit %>% 
   left_join(sales_count_2017_2018) %>% 
   left_join(sales_quantity_2017_2018) 
-  # mutate(average_sale = sum_quantity / sales_count) %>% 
-  # mutate(average_profit = sum_total_profit / quantity)
+
 
 # summary for 2017
 # sales_count
@@ -100,76 +100,17 @@ summary_2018 <- sales_sum_profit_2018 %>%
   left_join(sales_quantity_2018) %>% 
   arrange(desc(sum_total_profit))
 
-View(summary_2018)
+# View(summary_2018)
 
 profit_2017 <- sum(summary_2017$sum_total_profit)
 profit_2018 <- sum(summary_2018$sum_total_profit)
 # print(profit_2017)
 # print(profit_2018)
 
-View(summary_2017)
-View(summary_2018)
-
-summary_2018 <- summary_2018 %>% 
-  left_join(sales_data_2018, by="item_name")  %>% 
-  select(item_name, sum_total_profit, sales_count, sum_quantity, unit_buying_price, 
-         unit_selling_price, unit_price_margin) %>% 
-  distinct(item_name)
-
-View(summary_2018)
-
-
-# # compare 2017 and 2018 (2018 - 2017)
-# 
-# df <- summary_2017 %>% 
-#   left_join(summary_2018, by="item_name")
-# df[is.na(df)] = 0
-# 
-# diff_2018_2017 = data.frame()
-# diff_2018_2017 <- df %>% 
-#   mutate(difference_total_profit = df$sum_total_profit.y - df$sum_total_profit.x) %>% 
-#   mutate(difference_sales_count = df$sales_count.y - df$sales_count.x) %>% 
-#   mutate(difference_quantity = df$quantity.y - df$quantity.x) %>% 
-#   select(item_name,
-#          difference_total_profit,
-#          difference_sales_count,
-#          difference_quantity)
-# 
-# View(diff_2018_2017)
-# 
-# item_decreased <- diff_2018_2017 %>% 
-#   filter(difference_total_profit < 0)
-# 
-# quantile(item_decreased$difference_total_profit)
-# 
-# item_increased <- diff_2018_2017 %>% 
-#   filter(difference_total_profit > 0)
-# 
-# quantile(item_increased$difference_total_profit)
 
 
 
-
-
-# # by time? maybe?
-# # in 2017 04/11/2017 
-# 
-# # in 2017
-# sales_data_2017_time <- sales_data_2017 %>%
-#   filter(year == 2017, month_number == 4, day_of_week_name == 'Tuesday', week_number == 15) %>%
-#   group_by(hour) %>%
-#   summarise(count = n())
-# 
-# View(sales_data_2017_time)
-# 
-# # in 2018
-# sales_data_2018_time <- sales_data_2018 %>%
-#   group_by(hour) %>%
-#   summarise(count = n())
-# 
-# View(sales_data_2018_time)
-
-
+# sales per item dataset (distinct items, its information and its total profit)
 sales_per_item <- sales_data_2017_2018 %>% 
   group_by(item_name) %>% 
   summarize(num_sales = n()) %>% 
@@ -180,8 +121,7 @@ sales_per_item <- sales_data_2017_2018 %>%
 
 View(sales_per_item)
 
-quantile(sales_per_item$num_sales, 0.05)
-
+quantile(sales_per_item$num_sales, 0.01)
 
 
 # stop selling items with 1) quantity of 1 and 2) top 10 lowest 
@@ -195,7 +135,7 @@ View(stop_selling)
 
 
 # unit_selling_price - unit_price_margin = unit_buying_price
-# unit_buying_price for cabbage = 3.75
+
 
 # The highest selling products by month and category
 highest_selling <- sales_data_2017_2018 %>% 
@@ -213,19 +153,13 @@ lowest_selling <- sales_data_2017_2018 %>%
   group_by(month_number, main_category) %>% 
   filter(sales_count == min(sales_count))
 
-View(lowest_selling)  
-
-least_count <- lowest_selling %>% 
-  group_by(month_number, main_category) %>% 
-  summarise(item_count = n())
-
-View(least_count)
-
-df <- lowest_selling %>% 
+# because there is too many items with 1 sale, only shows the number of items
+# in each category
+lowest_selling <- lowest_selling %>% 
   group_by(month_number, main_category, sales_count) %>% 
   summarise(item_count = n())
 
-View(df)
+View(lowest_selling)
   
 
   
