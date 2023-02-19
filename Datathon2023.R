8926d5926e2d7b5eca90616a3b87a7fc1361788d
DLJ949 <djohnson949@yahoo.com>
2023-02-19 10:38:39 -0800

    Added new functions to find profit-related data

--- Datathon2023.R
+++ Datathon2023.R
@@ -77,7 +77,6 @@ new_df %>%
   summarise(avg_profit = mean(new_price))
 
 
-
 ## store's average profit per item (Any item)
 mean(new_df$total_profit)
 
@@ -122,5 +121,79 @@ number_sold <- another_data %>%
   group_by(item_name) %>% 
   summarise(n = length(item_name))
 
-# Quantiles of number of products sold
+# Quarantines of number of products sold
 quantile(number_sold$n)
+
+###################################################################
+### WHAT WE SHOULD EDIT FOR PRICES ###
+###################################################################
+
+# We CERTAINLY need to increase the price on the single most sold
+# item at All Foods, the Banana Cavendish. We may want to increase
+# the price of the second most sold item, the Field Tomatoes, if
+# the owners wish to increase its below-average profit margins.
+# The avergae profit margin for items at All Foods is $1.90.
+
+
+# Cabbage loses $2.25 when a person buys a whole cabbage for $1.50.
+# We want to increase the price by $3.00 such that we not only
+# avoid losing money during a transaction, but earn $0.47.
+
+
+
+### ACCORDING TO A MARCH 2009 REPORT FROM THE USDA ON "SUPERMARKET
+### LOSS ESTIMATES FOR FRESH FRUIT, VEGTABLES, MEAT, POULTRY, AND
+### SEAFOOD AND THEIR USE IN THE ERS LOSS-ADJUSTED FOOD
+### AVALIABILITY DATA":
+# https://www.ers.usda.gov/webdocs/publications/44306/10895_eib44.pdf
+
+# Before 2009, fresh food profit estimates were all in the red with
+# -12% profit margins across the board for all veggies and fruit
+# and -7% for all types of meat. This study, however, discovered
+# that profit margins ranged from -0.6% in sweet corn to -63.6% in
+# mustard greens (in 2009). Supermarkets use low prices in their
+# fresh foods and grocery departments to lure in customers and 
+# avoid foods expiring, taking a total loss.
+
+### ACCORDING TO THE FOOD INDUSTRY ASSOCIATION (FMI)'S "GROCERY
+### STORE CHAINS NET PROFITS":
+# https://www.fmi.org/our-research/supermarket-facts/grocery-store-chains-net-profit
+
+# Grocery Store chains historically have income profits that vary
+# between 1.1 and 3.5% in the United States, with the net profits
+# being a smaller percentage afterwards. The net profits of grocery
+# stores in the U.S. between 1984 and 2021 vary between 0.49 and
+# 3.0%. Currently because of the COVID 19 pandemic, income profits
+# and net profit percetnages are higher than ever.
+
+# The mean average profit rate for a grocery store is 2.2%.
+
+
+###################################################################
+# Function for finding profit margins by department.
+profit_margins_by_department <- another_data %>% 
+  group_by(main_category, sub_category) %>% 
+  summarise(avg_profit = mean(total_profit)) %>% 
+  arrange(avg_profit)
+  
+
+length(profit_margins$item_name) #1,136 unique items
+
+
+# Function that finds how many unique "Other Vegies" there are and
+# the number of them sold.
+another_data %>% 
+  group_by(item_name) %>% 
+  filter(sub_category == "Other Vegies") %>% 
+  summarise(n = length(item_name))
+
+# Total revenue earned by All Foods in 2 years
+total_revenue <- round(sum(another_data$total_profit), 2)
+
+# Function that finds PERCENTAGE of revenue per department and
+# sub-category
+percentage_profit_margins_dep <- another_data %>% 
+  group_by(main_category, sub_category) %>% 
+  summarise(percentage = round((sum(total_profit)/total_revenue)*100, 4))
+
+sum(percentage_profit_margins_dep$percentage)
