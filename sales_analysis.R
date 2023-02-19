9a3bfee215369592e895a060ca47c07cf10389d5
Sunghee Park <sungheepark@Sunghees-MacBook-Pro.local>
2023-02-18 22:46:09 -0800

    updated

--- sales_analysis.R
+++ sales_analysis.R
@@ -1,7 +1,8 @@
 library(dplyr)
 library(readr)
 sales_data_2017_2018 <- read_csv("Documents/datathon2023/CSV-Datathon/new_dataset.csv")
-
+library(data.table)
+library(ggplot2)
 # stop selling when 
 # 1 sell 1-10 item per year?
 # profit is less than a certain amount?
@@ -18,36 +19,44 @@ sales_data_2018 <- sales_data_2017_2018 %>%
 
 # View(sales_data_2018)
 
-sales_data_2017 <- sales_data_2017 %>%
+# data only sold in 2017 
+
+sales_data_2017_ <- sales_data_2017 %>%
   group_by(item_name) %>%
-  summarise(sales_count = n()) %>%
+  summarise(sum_quantity = sum(quantity)) %>%
   arrange()
 
-# View(sales_data_2017)
+View(sales_data_2017_)
 
-sales_data_2018 <- sales_data_2018 %>%
+sales_data_2018_ <- sales_data_2018 %>%
   group_by(item_name) %>%
-  summarise(sales_count = n()) %>%
+  summarise(sum_quantity = sum(quantity)) %>%
   arrange()
 
-# View(sales_data_2018)
-
-item_count_2017 <- sales_data_2017[sales_data_2017$sales_count <= 10,]
-item_count_2018 <- sales_data_2018[sales_data_2018$sales_count <= 10,]
-
-# View(item_count_2017)
-# View(item_count_2018)
+View(sales_data_2018_)
 
 # items that are only sold in 2017 -- not correct do not use
-items_both <- item_count_2017 %>% 
-  right_join(item_count_2018)
-# View(items_both)
+items_both <- sales_data_2017_ %>% 
+  right_join(sales_data_2018_)
+View(items_both)
+
+only_2017 <- sales_data_2017 %>%
+  filter(!(items_both$item_name %in% sales_data_2017_$item_name))
+
+
+
 
 # total profit per item & num count for each
 sales_count_2017_2018 <- sales_data_2017_2018 %>% 
   group_by(item_name) %>% 
   summarise(sales_count = n())
 # View(sales_count_2017_2018)
+
+sales_quantity_2017_2018 <- sales_data_2017_2018 %>% 
+  group_by(item_name) %>% 
+  summarize(quantity = sum(quantity))
+
+View(sales_quantity_2017_2018)
 
 sum_profit <- sales_data_2017_2018 %>% 
   group_by(item_name) %>% 
@@ -56,26 +65,43 @@ sum_profit <- sales_data_2017_2018 %>%
 # View(sum_profit)
 
 
-
 joined <- sum_profit %>% 
   left_join(sales_count_2017_2018) %>% 
-  mutate(average_profit = sum_total_profit / sales_count)
+  left_join(sales_quantity_2017_2018) %>% 
+  mutate(average_sale = quantity / sales_count) %>% 
+  mutate(average_profit = sum_total_profit / quantity)
+  
 
-View(joined)
+sum_quantity = sum(joined$quantity)
+
+quantile(joined$quantity)
+
+# Banana Cavendish increase 
+#
+
 
 
 # by time? maybe?
-# # in 2017 04/11/2017 
-# sales_data_2017_time <- sales_data_2017 %>% 
-#   filter(year == 2017, month_number == 4, day_of_week_name == 'Tuesday', week_number == 15) %>% 
-#   group_by(hour) %>% 
+# in 2017 04/11/2017 
+# df <- read.csv("Documents/datathon2023/sales_data_2017_2018_for_tableau_with_new_date_columns.csv")
+
+# sales_data_2017 <- df %>% 
+#   filter(year == 2017)
+# 
+# sales_data_2018 <- df %>% 
+#   filter(year == 2018)
+# 
+# # in 2017
+# sales_data_2017_time <- sales_data_2017 %>%
+#   filter(year == 2017, month_number == 4, day_of_week_name == 'Tuesday', week_number == 15) %>%
+#   group_by(hour) %>%
 #   summarise(count = n())
 # 
 # View(sales_data_2017_time)
 # 
 # # in 2018
-# sales_data_2018_time <- sales_data_2018 %>% 
-#   group_by(hour) %>% 
+# sales_data_2018_time <- sales_data_2018 %>%
+#   group_by(hour) %>%
 #   summarise(count = n())
 # 
 # View(sales_data_2018_time)
